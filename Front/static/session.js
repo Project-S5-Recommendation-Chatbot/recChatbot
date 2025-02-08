6ca237f64706b8c6d105bb76e1bde7e6cc559243
Arman Karapetyan <39801300+thearmankarapetyan@users.noreply.github.com>
2025-02-08 17:29:02 +0400

    Update session.js

--- Front/static/session.js
+++ Front/static/session.js
@@ -6,21 +6,21 @@ document.addEventListener('DOMContentLoaded', () => {
         loginLink.href = '#'; // Prevent navigation back to the login page.
     }
 });
-document.addEventListener('DOMContentLoaded', function() {
-    const loginLink = document.getElementById('loginLink');
-    const user = sessionStorage.getItem('user');
+// document.addEventListener('DOMContentLoaded', function() {
+//     const loginLink = document.getElementById('loginLink');
+//     const user = sessionStorage.getItem('user');
 
-    if (loginLink) {
-        if (user) {
-            loginLink.textContent = user + " - Logout";
-            loginLink.href = 'javascript:void(0);'; // Prevent default link behavior
-            loginLink.onclick = logout; // Assign logout function
-        } else {
-            loginLink.textContent = 'Login';
-            loginLink.href = '#';
-            loginLink.onclick = null; // Ensure no logout function is attached
-        }
-    }
+//     if (loginLink) {
+//         if (user) {
+//             loginLink.textContent = user + " - Logout";
+//             loginLink.href = 'javascript:void(0);'; // Prevent default link behavior
+//             loginLink.onclick = logout; // Assign logout function
+//         } else {
+//             loginLink.textContent = 'Login';
+//             loginLink.href = '#';
+//             loginLink.onclick = null; // Ensure no logout function is attached
+//         }
+//     }
 
     function logout() {
         sessionStorage.removeItem('user');
