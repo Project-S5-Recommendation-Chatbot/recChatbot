936e4da12f582bdafcab8e8e31b49f1f524162f3
Arman Karapetyan <39801300+thearmankarapetyan@users.noreply.github.com>
2025-02-08 16:57:53 +0400

    Update session.js

--- Front/static/session.js
+++ Front/static/session.js
@@ -1,26 +1,26 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//     const user = sessionStorage.getItem('user');
-//     const loginLink = document.getElementById('loginLink');
-//     if (user && loginLink) {
-//         loginLink.textContent = user;
-//         loginLink.href = '#'; // Prevent navigation back to the login page.
-//     }
-// });
-document.addEventListener('DOMContentLoaded', function() {
-    const loginLink = document.getElementById('loginLink');
+document.addEventListener('DOMContentLoaded', () => {
     const user = sessionStorage.getItem('user');
-
-    if (loginLink) {
-        if (user) {
-            loginLink.textContent = user + " - Logout";
-            loginLink.href = 'javascript:void(0);'; // Prevent default link behavior
-            loginLink.onclick = logout; // Assign logout function
-        } else {
-            loginLink.textContent = 'Login';
-            loginLink.href = 'login.html';
-            loginLink.onclick = null; // Ensure no logout function is attached
-        }
+    const loginLink = document.getElementById('loginLink');
+    if (user && loginLink) {
+        loginLink.textContent = user;
+        loginLink.href = '#'; // Prevent navigation back to the login page.
     }
+});
+// document.addEventListener('DOMContentLoaded', function() {
+//     const loginLink = document.getElementById('loginLink');
+//     const user = sessionStorage.getItem('user');
+
+//     if (loginLink) {
+//         if (user) {
+//             loginLink.textContent = user + " - Logout";
+//             loginLink.href = 'javascript:void(0);'; // Prevent default link behavior
+//             loginLink.onclick = logout; // Assign logout function
+//         } else {
+//             loginLink.textContent = 'Login';
+//             loginLink.href = 'login.html';
+//             loginLink.onclick = null; // Ensure no logout function is attached
+//         }
+//     }
 
     function logout() {
         sessionStorage.removeItem('user');
